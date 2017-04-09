93bbd006e6c0bf02e051d0f3ae7dc7cfdc6eb330
nsaravanas <saravanagceb@gmail.com>
2017-04-09 20:55:52 +0530

    repo annotation added

--- src/main/java/com/eats/repository/UserRepository.java
+++ src/main/java/com/eats/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package com.eats.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.eats.models.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-	
+
 }
